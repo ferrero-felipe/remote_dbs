84dda16efd0b5be65d242d92147331edb10f6d75
ferrero-felipe <ferrero.felipe@gmail.com>
2019-11-29 10:45:00 +0100

    new dataset

--- my-application/sql/populate.py
+++ my-application/sql/populate.py
@@ -13,24 +13,65 @@ conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 #Create Cursor
 cur = conn.cursor()
 #Create Tables
-query = """CREATE TABLE IF NOT EXISTS chats (
-  idchats INT PRIMARY KEY,
-  source VARCHAR(45) NULL,
+query = """
+CREATE TABLE IF NOT EXISTS User (
+  idUsers INT NOT NULL,
+  userName VARCHAR(45) NOT NULL,
+  PRIMARY KEY (idUsers));
+CREATE TABLE IF NOT EXISTS Chat (
+  idChat INT NOT NULL,
+  PRIMARY KEY (idChat));
+CREATE TABLE IF NOT EXISTS Message (
+  idMessage INT NOT NULL,
   text VARCHAR(45) NULL,
-  date VARCHAR(45) NULL,
-  message_id VARCHAR(45) NULL,
-  chat_id VARCHAR(45) NULL);"""
+  datetime VARCHAR(45) NULL
+  User_idUsers INT NOT NULL,
+  Chat_idChat INT NOT NULL,
+  PRIMARY KEY (idMessage),
+  INDEX fk_Message_User_idx (User_idUsers ASC),
+  INDEX fk_Message_Chat1_idx (Chat_idChat ASC),
+  CONSTRAINT fk_Message_User
+    FOREIGN KEY (User_idUsers)
+    REFERENCES User (idUsers)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_Message_Chat1
+    FOREIGN KEY (Chat_idChat)
+    REFERENCES Chat (idChat)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)"""
 cur.execute(query)
 #Populate Tables
-chats = pd.read_csv('my-application/input/clean_chats.csv')
-query = "INSERT INTO chats VALUES {} RETURNING message_id"
-for _,row in chats.iterrows():
+chats = pd.read_json('my-application/input/chats.json',orient='records')
+query = "INSERT INTO {} VALUES {} RETURNING {}"
+with open('my-application/input/chats.json') as f:
+    chats_json = json.load(f)
+users = list(set([(chats_json[i]['idUser'],chats_json[i]['userName']) for i in range(len(chats_json))]))
+chats = list(set([(chats_json[i]['idChat']) for i in range(len(chats_json))]))
+for user in users:
   try:
-    values = str(tuple(row.values))
-    cur.execute(query.format(values))
+    cur.execute(query.format('User',str(user),'idUser'))
     #Get Response
-    message_id = cur.fetchone()[0]
-    print(f"message inserted: {message_id}")
+    id = cur.fetchone()[0]
+    print(f"value inserted: {id}")
+  except:
+    print("At least I tried")
+for chat in chats:
+  try:
+    cur.execute(query.format('Chat',str(chat),'idChat'))
+    #Get Response
+    id = cur.fetchone()[0]
+    print(f"value inserted: {id}")
+  except:
+    print("At least I tried")
+for message in chats_json:
+  try:
+    cur.execute(query.format('Message',"({},{},{},{},{})".format(message['idMessage'],message['text'],message['datetime'],message['idUsers'],message['idChat'],),'idMessage'))
+    #Get Response
+    id = cur.fetchone()[0]
+    print(f"value inserted: {id}")
   except:
     print("At least I tried")
 print('Done!')
+
+
