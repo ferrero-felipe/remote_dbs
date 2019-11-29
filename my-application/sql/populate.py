7f011dc651c1471d400708f6f78c774f23a4128b
ferrero-felipe <ferrero.felipe@gmail.com>
2019-11-29 11:50:25 +0100

    fixing errors

--- my-application/sql/populate.py
+++ my-application/sql/populate.py
@@ -49,24 +49,30 @@ with open('my-application/input/chats.json') as f:
 users = list(set([(chats_json[i]['idUser'],chats_json[i]['userName']) for i in range(len(chats_json))]))
 chats = list(set([(chats_json[i]['idChat']) for i in range(len(chats_json))]))
 for user in users:
+  q = query.format('users (idUser, userName)',"({}, '{}')".format(user[0],user[1]),'users.idUser')
+  print(q)
   try:
-    cur.execute(query.format('users (idUser, userName)',"({}, '{}')".format(user[0],user[1]),'users.idUser'))
+    cur.execute(q)
     #Get Response
     id = cur.fetchone()[0]
     print(f"value inserted: {id}")
   except:
     print("At least I tried")
 for chat in chats:
+  q = query.format('chats(idChat)',"({})".format(chat[0]),'chats.idChat')
+  print(q)
   try:
-    cur.execute(query.format('chats(idChat)',"({})".format(chat[0]),'chats.idChat'))
+    cur.execute(q)
     #Get Response
     id = cur.fetchone()[0]
     print(f"value inserted: {id}")
   except:
     print("At least I tried")
 for message in chats_json:
+  q = query.format('chats(idChat)',"({})".format(chat[0]),'chats.idChat')
+  print(q)
   try:
-    cur.execute(query.format('messages(idMessage, text, datetime, users_idUser, chats_idChat)',"({},'{}','{}',{},{})".format(message['idMessage'],message['text'],message['datetime'],message['idUsers'],message['idChat'],),'messages.idMessage'))
+    cur.execute(q)
     #Get Response
     id = cur.fetchone()[0]
     print(f"value inserted: {id}")
