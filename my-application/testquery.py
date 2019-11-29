57b7ea256feb7952065b840f38c9f0fbb09f2335
ferrero-felipe <ferrero.felipe@gmail.com>
2019-11-29 06:04:06 +0100

    adding clear and testquery functions

---
+++ my-application/testquery.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import os
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT 
+
+DATABASE_URL = os.environ['DATABASE_URL']
+#Connect to DB
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+#If permission Error
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+#Create Cursor
+cur = conn.cursor()
+#Query Data
+query = """SELECT * FROM chats;"""
+cur.execute(query)
+result = cur.fetchall()
+print(result)
