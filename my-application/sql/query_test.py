28e485079852fb4260f290b47841611ad6b16711
ferrero-felipe <ferrero.felipe@gmail.com>
2019-11-29 14:11:42 +0100

    mm

--- my-application/sql/query_test.py
+++ my-application/sql/query_test.py
@@ -3,6 +3,12 @@
 import os
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT 
+import argparse
+
+parser = argparse.ArgumentParser(description='Get messages for specific idChat')
+parser.add_argument('--id', type=int, help='idChat')
+n = parser.parse_args().id
+print(n)
 
 DATABASE_URL = os.environ['DATABASE_URL']
 #Connect to DB
@@ -12,7 +18,7 @@ conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 #Create Cursor
 cur = conn.cursor()
 #Query Data
-query = """SELECT * FROM messages WHERE chats_idChat=0;"""
+query = """SELECT * FROM messages WHERE chats_idChat={};""".format(n)
 cur.execute(query)
 result = cur.fetchall()
 print(result)
